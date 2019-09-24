4159203976f9c6b836996f53cafef4553af3b296
yash1190 <55666851+yash1190@users.noreply.github.com>
2019-09-24 23:31:56 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+name = input('Enter your name : ')
+a = int(input('Enter your age : '))
+b = str(2019+(100-a))
+statement = "Hey {}! You will be 100 years old in the year {}"
+statement.format('Arya',b)
+print("statement")
+
+
+
